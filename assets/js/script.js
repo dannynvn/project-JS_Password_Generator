9e24e4456b2d0c46a8564e115926cba27a111887
Danny Nguyen <dannynvn@gmail.com>
2023-01-13 19:11:34 -0800

    added summary confirmation that lists the user password criteria determined by user prompts; added comments to organize code

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,12 +13,11 @@ function writePassword() {
   function generatePassword() {
     //prompt user for amount of characters to include for password length; minimum is 8; maximum is 128
     var passwordLength = Number(prompt("How many characters would you like your password to contain? (min: 8 characters / max: 128 characters)"));
-
+    //notifies user if they entered a value outside of min/max range
     if (passwordLength < 8 || passwordLength > 128) {
       return "Please input a value between 8 and 128";
     } 
     
-
     //prompts user for characters to include in generated password
     var lowercaseAlphaConfirm = window.confirm("Would you like to include lowercase alphabet letters?");
     var uppercaseAlphaConfirm = window.confirm("Would you like to include uppercase alphabet letters?");
@@ -53,11 +52,15 @@ function writePassword() {
     } 
     if (specialCharConfirm == true) {
       passwordCharacters = [...passwordCharacters, ...specialChar];
-    }  
+    } 
+    //if statement to notify user that they must select at least 1 criteria 
     if (lowercaseAlphaConfirm == false && uppercaseAlphaConfirm == false && numericCharConfirm == false && specialCharConfirm == false) {
       window.confirm("You must select at least 1 password character criteria.");
       return "Please regenerate password";
     }
+
+    //summarize user password criteria determined by prompts
+    window.confirm(`Thank you for confirming. Your password criteria is set as the following: \nCharacter length: ${passwordLength} \nLowercase alphabet: ${lowercaseAlphaConfirm} \nUppercase alphabet: ${uppercaseAlphaConfirm} \nNumbers: ${numericCharConfirm} \nSpecial characters: ${specialCharConfirm}`);
 
     //randomly choose and append characters into one final password string
     var passwordString = ''
