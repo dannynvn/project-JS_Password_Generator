0b48ae06db3c7c4a0ecf77732990cf6235c4e5ac
Danny Nguyen <dannynvn@gmail.com>
2023-01-13 16:20:21 -0800

    added code to generate 1 random character

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,7 +10,19 @@ function writePassword() {
 
   passwordText.value = password;
 
+  function generatePassword() {
+    passwordCharacters = '0123456789abcdefghijklmnopqrstuvwxyz!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~'.split (
+      ''
+    );
+    var random = Math.floor(Math.random() * passwordCharacters.length);
+
+    return passwordCharacters[random];
+
+  }
+  
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
