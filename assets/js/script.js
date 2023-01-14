8c1471c4a1875a2c67716010cce5abad1c59f744
Danny Nguyen <dannynvn@gmail.com>
2023-01-13 17:51:03 -0800

    added while loop to check if password length value is between 8 and 128

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,31 @@ function writePassword() {
 
   passwordText.value = password;
 
+  //prompt user for amount of characters to include for password length; minimum is 8; maximum is 128
+  var passwordLength = Number(prompt("How many characters would you like your password to contain? (min: 8 characters / max: 128 characters)"));
+  var passLengthCheck = false;
+  while(passLengthCheck == false) {
+    var passwordLength = Number(prompt("Please enter a value between 8 and 128."));
+    if (passwordLength < 8 || passwordLength > 128) {
+      passLengthCheck = false;
+    } else {
+      passLengthCheck = true;
+    }
+  } 
+
+  // //prompt user if they'd like to include lowercase alphabet characters
+  // var lowercaseAlphabet = window.confirm("Would you like to include lowercase alphabet letters?");
+
+  // //prompt user if they'd like to include uppercase alphabet characters
+  // var uppercaseAlphabet = window.confirm("Would you like to include uppercase alphabet letters?");
+
+  // //prompt user if they'd like to include numeric characters
+  // var numericChar = window.confirm("Would you like to include numeric characters?");
+
+  // //prompt user if they'd like to include special characters
+  // var specialChar = window.confirm("Would you like to include special characters?");
+
+
   function generatePassword() {
     passwordCharacters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~'.split (
       ''
@@ -23,7 +48,7 @@ function writePassword() {
     return passwordString;
 
   }
-  
+
 }
 
 
