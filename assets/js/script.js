1a58a6ab47e7ee5d2dbe5c95c9854ff1d0ac49e8
Danny Nguyen <dannynvn@gmail.com>
2023-01-13 17:00:51 -0800

    added code to generate a random string with more than 1 character

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,16 @@ function writePassword() {
   passwordText.value = password;
 
   function generatePassword() {
-    passwordCharacters = '0123456789abcdefghijklmnopqrstuvwxyz!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~'.split (
+    passwordCharacters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~'.split (
       ''
     );
-    var random = Math.floor(Math.random() * passwordCharacters.length);
-
-    return passwordCharacters[random];
+    var passwordString = ''
+    
+    for (var i = 0; i < 20; i++) {
+      var random = Math.floor(Math.random() * passwordCharacters.length);
+      passwordString += passwordCharacters[random];
+    }
+    return passwordString;
 
   }
   
