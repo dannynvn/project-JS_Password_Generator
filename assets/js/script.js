a17a409943e357a92e73ccc00c53599860c505c7
Danny Nguyen <dannynvn@gmail.com>
2023-01-13 18:01:50 -0800

    connected user prompted password length value to how many characters are generated

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,38 +10,37 @@ function writePassword() {
 
   passwordText.value = password;
 
-  //prompt user for amount of characters to include for password length; minimum is 8; maximum is 128
-  var passwordLength = Number(prompt("How many characters would you like your password to contain? (min: 8 characters / max: 128 characters)"));
-  var passLengthCheck = false;
-  while(passLengthCheck == false) {
-    var passwordLength = Number(prompt("Please enter a value between 8 and 128."));
-    if (passwordLength < 8 || passwordLength > 128) {
-      passLengthCheck = false;
-    } else {
-      passLengthCheck = true;
-    }
-  } 
-
-  // //prompt user if they'd like to include lowercase alphabet characters
-  // var lowercaseAlphabet = window.confirm("Would you like to include lowercase alphabet letters?");
-
-  // //prompt user if they'd like to include uppercase alphabet characters
-  // var uppercaseAlphabet = window.confirm("Would you like to include uppercase alphabet letters?");
-
-  // //prompt user if they'd like to include numeric characters
-  // var numericChar = window.confirm("Would you like to include numeric characters?");
-
-  // //prompt user if they'd like to include special characters
-  // var specialChar = window.confirm("Would you like to include special characters?");
-
-
   function generatePassword() {
+    //prompt user for amount of characters to include for password length; minimum is 8; maximum is 128
+    var passwordLength = Number(prompt("How many characters would you like your password to contain? (min: 8 characters / max: 128 characters)"));
+
+    // var passLengthCheck = false;
+    // while(passLengthCheck == false) {
+    //   if (passwordLength < 8 || passwordLength > 128) {
+    //     passLengthCheck = false;
+    //   } else {
+    //     passLengthCheck = true;
+    //   }
+    // } 
+
+    //prompt user if they'd like to include lowercase alphabet characters
+    var lowercaseAlphabet = window.confirm("Would you like to include lowercase alphabet letters?");
+    //prompt user if they'd like to include uppercase alphabet characters
+    var uppercaseAlphabet = window.confirm("Would you like to include uppercase alphabet letters?");
+    //prompt user if they'd like to include numeric characters
+    var numericChar = window.confirm("Would you like to include numeric characters?");
+    //prompt user if they'd like to include special characters
+    var specialChar = window.confirm("Would you like to include special characters?");
+      
+
+    //create an array of all possible password characters
     passwordCharacters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~'.split (
       ''
     );
-    var passwordString = ''
     
-    for (var i = 0; i < 20; i++) {
+    //randomly choose and append characters into one final password string
+    var passwordString = ''
+    for (var i = 0; i < passwordLength; i++) {
       var random = Math.floor(Math.random() * passwordCharacters.length);
       passwordString += passwordCharacters[random];
     }
